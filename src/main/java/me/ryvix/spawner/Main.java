a965094e1f58f1358e0a2f3dcbaf7fb09a30f271
Jerethkhan <jereth@hotmail.com>
2014-12-06 23:50:06 -0400

    Update Main.java
    Adding Endermites, Guardians, and Rabbit support for 1.8

--- src/main/java/me/ryvix/spawner/Main.java
+++ src/main/java/me/ryvix/spawner/Main.java
@@ -107,7 +107,7 @@ public class Main extends JavaPlugin {
 
 		// add defaults
 		if (!config.contains("valid_entities")) {
-			List<String> validEntities = Arrays.asList("Creeper", "Skeleton", "Spider", "Giant", "Zombie", "Slime", "Ghast", "PigZombie", "Enderman", "CaveSpider", "Silverfish", "Blaze", "LavaSlime", "EnderDragon", "WitherBoss", "Bat", "Witch", "Pig", "Sheep", "Cow", "Chicken", "Squid", "Wolf", "MushroomCow", "SnowMan", "Ozelot", "VillagerGolem", "EntityHorse", "Villager");
+			List<String> validEntities = Arrays.asList("Creeper", "Skeleton", "Spider", "Giant", "Zombie", "Slime", "Ghast", "PigZombie", "Enderman", "CaveSpider", "Silverfish", "Blaze", "LavaSlime", "EnderDragon", "WitherBoss", "Bat", "Witch", "Pig", "Sheep", "Cow", "Chicken", "Squid", "Wolf", "MushroomCow", "SnowMan", "Ozelot", "VillagerGolem", "EntityHorse", "Villager", "Endermite", "Guardian", "Rabbit");
 			getConfig().addDefault("valid_entities", validEntities);
 		}
 		if (config.contains("bad_entities")) {
